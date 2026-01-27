3d2387d234820d1ac7604429d06c52d1bf11a8b4
JayemCeekay <mrjmck95@gmail.com>
2026-01-27 08:45:33 -0600

    Refactor `MixinShowdownMovesetAdapter` to adjust the wrapping and filtering of shadow moves during deserialization and gimmick-move mapping

--- common/src/main/java/com/jayemceekay/shadowedhearts/mixin/MixinShowdownMovesetAdapter.java
+++ common/src/main/java/com/jayemceekay/shadowedhearts/mixin/MixinShowdownMovesetAdapter.java
@@ -3,41 +3,56 @@ package com.jayemceekay.shadowedhearts.mixin;
 import com.cobblemon.mod.common.battles.InBattleMove;
 import com.cobblemon.mod.common.battles.ShowdownMoveset;
 import com.cobblemon.mod.common.battles.ShowdownMovesetAdapter;
-import com.google.gson.JsonDeserializationContext;
-import com.google.gson.JsonElement;
 import com.jayemceekay.shadowedhearts.ShadowGate;
-import com.llamalad7.mixinextras.sugar.Local;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import kotlin.Unit;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
 
 @Mixin(value = ShowdownMovesetAdapter.class, remap = false)
 public abstract class MixinShowdownMovesetAdapter {
 
-    @Inject(
+    @WrapOperation(
             method = "deserialize(Lcom/google/gson/JsonElement;Ljava/lang/reflect/Type;Lcom/google/gson/JsonDeserializationContext;)Lcom/cobblemon/mod/common/battles/ShowdownMoveset;",
             at = @At(value = "INVOKE", target = "Lcom/cobblemon/mod/common/battles/ShowdownMoveset;setGimmickMapping()Lkotlin/Unit;")
     )
-    private void shadowedhearts$beforeSetGimmickMapping(
-            JsonElement jsonElement,
-            Type type,
-            JsonDeserializationContext context,
-            CallbackInfoReturnable<ShowdownMoveset> cir,
-            @Local(name = "moveset") ShowdownMoveset moveset
-    ) {
-        if (moveset.getMoves() != null) {
-            List<InBattleMove> filteredMoves = new ArrayList<>();
-            for (InBattleMove move : moveset.getMoves()) {
-                if (!ShadowGate.isShadowMoveId(move.getId())) {
-                    filteredMoves.add(move);
-                }
-            }
-            moveset.setMoves(filteredMoves);
+    private Unit shadowedhearts$wrapSetGimmickMapping(ShowdownMoveset instance, Operation<Unit> original) {
+        List<InBattleMove> originalMoves = instance.getMoves();
+        if (originalMoves != null) {
+            instance.setMoves(shadowedhearts$getFilteredMoves(originalMoves));
+            Unit result = original.call(instance);
+            instance.setMoves(originalMoves);
+            return result;
         }
+        return original.call(instance);
+    }
+
+    @WrapOperation(
+            method = "deserialize(Lcom/google/gson/JsonElement;Ljava/lang/reflect/Type;Lcom/google/gson/JsonDeserializationContext;)Lcom/cobblemon/mod/common/battles/ShowdownMoveset;",
+            at = @At(value = "INVOKE", target = "Lcom/cobblemon/mod/common/battles/ShowdownMoveset;setMaxMoves(Ljava/util/List;)V")
+    )
+    private void shadowedhearts$wrapSetMaxMoves(ShowdownMoveset instance, List<?> maxMoves, Operation<Void> original) {
+        List<InBattleMove> originalMoves = instance.getMoves();
+        if (originalMoves != null) {
+            instance.setMoves(shadowedhearts$getFilteredMoves(originalMoves));
+            original.call(instance, maxMoves);
+            instance.setMoves(originalMoves);
+        } else {
+            original.call(instance, maxMoves);
+        }
+    }
+
+    private List<InBattleMove> shadowedhearts$getFilteredMoves(List<InBattleMove> moves) {
+        List<InBattleMove> filteredMoves = new ArrayList<>();
+        for (InBattleMove move : moves) {
+            if (!ShadowGate.isShadowMoveId(move.getId())) {
+                filteredMoves.add(move);
+            }
+        }
+        return filteredMoves;
     }
 }
