35cc029626efa5c28cffd7e7efb910454003aadf
JayemCeekay <mrjmck95@gmail.com>
2026-02-08 10:54:12 -0600

    Adjust "Max Shadow" move effects to reduce stat stage changes from 2 to 1
    - Updated "Max Shadow" description to reflect the adjusted stat stage changes (-1 Defense/Special Defense, +1 Attack/Special Attack).
    - Modified `onHit` logic to apply the updated stage boosts/debuffs (-1 instead of -2, +1 instead of +2).

--- common/src/main/resources/data/shadowedhearts/showdown/moves/maxshadow.js
+++ common/src/main/resources/data/shadowedhearts/showdown/moves/maxshadow.js
@@ -2,7 +2,7 @@
     accuracy: true,
     basePower: 100,
     category: "Physical",
-    desc: "A Shadow-type attack that Dynamax Pokémon use. This lowers everyone's Defense and Special Defense by 2 stages, and raises everyone's Attack and Special Attack by 2 stages.",
+    desc: "A Shadow-type attack that Dynamax Pokémon use. This lowers everyone's Defense and Special Defense by 1 stage, and raises everyone's Attack and Special Attack by 1 stage.",
     name: "Max Shadow",
     pp: 10,
     priority: 0,
@@ -12,7 +12,7 @@
         onHit(source) {
             if (!source.volatiles['dynamax']) return;
             for (const pokemon of this.getAllActive()) {
-                this.boost({def: -2, spd: -2, atk: 2, spa: 2}, pokemon);
+                this.boost({def: -1, spd: -1, atk: 1, spa: 1}, pokemon);
             }
         },
     },
